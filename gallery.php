e654e5ed80a47128b61c161f8dc858f2d5b50aae
joyMajumder123 <atanumajumdero79@gmail.com>
2026-02-04 15:39:10 +0530

    added gallery images

---
+++ gallery.php
@@ -0,0 +1,30 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/navbar.php'; ?>
+
+<header class="hero-wrapper" style="background-image: url('https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=1600&q=80');">
+    <div class="hero-overlay"></div>
+    <div class="container hero-content">
+        <h1 class="hero-title">Our Gallery</h1>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb justify-content-center">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item active">Gallery</li>
+            </ol>
+        </nav>
+    </div>
+</header>
+
+<section class="py-5">
+    <div class="container">
+        <div class="row g-4">
+            <div class="col-md-4"><img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=500&q=80" class="img-fluid rounded shadow w-100" alt="Gallery"></div>
+            <div class="col-md-4"><img src="https://images.unsplash.com/photo-1559339352-11d035aa65de?w=500&q=80" class="img-fluid rounded shadow w-100" alt="Gallery"></div>
+            <div class="col-md-4"><img src="https://images.unsplash.com/photo-1552566626-52f8b828add9?w=500&q=80" class="img-fluid rounded shadow w-100" alt="Gallery"></div>
+            <div class="col-md-4"><img src="https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?w=500&q=80" class="img-fluid rounded shadow w-100" alt="Gallery"></div>
+            <div class="col-md-4"><img src="https://images.unsplash.com/photo-1551024709-8f23befc6f87?w=500&q=80" class="img-fluid rounded shadow w-100" alt="Gallery"></div>
+            <div class="col-md-4"><img src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?w=500&q=80" class="img-fluid rounded shadow w-100" alt="Gallery"></div>
+        </div>
+    </div>
+</section>
+
+<?php include 'includes/footer.php'; ?>
